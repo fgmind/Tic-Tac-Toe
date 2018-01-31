6aa64952239e8424941b5cea681dab2b7f188e5c
Francois <20587355+fgmind@users.noreply.github.com>
2018-01-31 13:45:11 +1300

    Add files via upload

---
+++ TicTacToe/TicTacToe/TicTacToeForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.newGame = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(13, 13);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(75, 75);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.btnClick);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(94, 13);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(75, 75);
+            this.a2.TabIndex = 0;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.btnClick);
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(175, 12);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(75, 75);
+            this.a3.TabIndex = 0;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.btnClick);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(13, 94);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 75);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.btnClick);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(94, 94);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 75);
+            this.b2.TabIndex = 0;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.btnClick);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(175, 93);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 75);
+            this.b3.TabIndex = 0;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.btnClick);
+            // 
+            // c1
+            // 
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(13, 174);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(75, 75);
+            this.c1.TabIndex = 0;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.btnClick);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(94, 174);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(75, 75);
+            this.c2.TabIndex = 0;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.btnClick);
+            // 
+            // c3
+            // 
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(175, 173);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(75, 75);
+            this.c3.TabIndex = 0;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.btnClick);
+            // 
+            // newGame
+            // 
+            this.newGame.Location = new System.Drawing.Point(13, 272);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(104, 38);
+            this.newGame.TabIndex = 1;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(146, 272);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(104, 38);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 322);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.newGame);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.MaximizeBox = false;
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button newGame;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/TicTacToeForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        bool turn = true;   // when true it is X's turn, if false O's turn
+        int turnCount = 0;  // maximum of turns is 9 before end of game
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)     // this closes the game
+        {
+            Close();
+        }
+
+        private void btnClick(object sender, EventArgs e)   // adds X or O, and increments count. For all buttons
+        {
+            Button b = (Button)sender;  // temp object to represent button
+
+            if (turn == true)   // X's turn
+            {
+                b.Text = "X";
+            }
+            else                // O's turn
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;       // changes player turn
+            b.Enabled = false;  // so that player cannot click on used button
+
+            turnCount++;        // increments count
+            checkIfWin();       // then checks for a win or draw
+
+        }
+
+        private void checkIfWin()   // checks whether player wins
+        {
+            bool gameWon = false;
+
+            foreach(Control c in Controls)  // checks all 8  winning combinations
+            {
+                if ((a1.Text == b1.Text) && (a1.Text == c1.Text) && !a1.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((a2.Text == b2.Text) && (a2.Text == c2.Text) && !a2.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((a3.Text == b3.Text) && (a3.Text == c3.Text) && !a3.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((a1.Text == a2.Text) && (a1.Text == a3.Text) && !a1.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((b1.Text == b2.Text) && (b1.Text == b3.Text) && !b1.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((c1.Text == c2.Text) && (c1.Text == c3.Text) && !c1.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((a1.Text == b2.Text) && (a1.Text == c3.Text) && !a1.Enabled)
+                {
+                    gameWon = true;
+                }
+                else if ((a3.Text == b2.Text) && (a3.Text == c1.Text) && !a3.Enabled)
+                {
+                    gameWon = true;
+                }
+            }
+            if (gameWon == true)
+            {
+                disableButtons();   // since game over player can't click on buttons anymore
+
+                String winner = "";
+
+                if (turn == true)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+
+                MessageBox.Show(winner +" is the winner!");
+            }
+
+            if (turnCount == 9)                     // if no-one wins by the end of count, Draw
+            {
+                MessageBox.Show("It's a draw!!!");
+                disableButtons();
+            }
+        }
+
+        private void disableButtons()
+        {
+            foreach (Control c in Controls)
+            {
+                Button b = (Button)c;
+                b.Enabled = false;          // all buttons are disabled!
+            }
+            newGame.Enabled = true;         // re-activating 'New Game' and 'exit'
+            exit.Enabled = true;
+        }
+
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+            {
+                Button b = (Button)c;
+                b.Enabled = true;       // re-activating buttons for new game
+                b.Text = "";            // to clear Xs and Os    
+            }
+            turn = true;                // go back to X's turn
+            turnCount = 0;              // reset counter
+            newGame.Text = "New Game";
+            exit.Text = "Exit";
+        }
+    }
+}
